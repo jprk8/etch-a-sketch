6eabe1a1e2be7f13da14db08f152d98791065eda
John Park <176120986+jprk8@users.noreply.github.com>
2024-07-28 02:01:04 -0700

    Fix resizing logic to change resolution

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const container = document.querySelector("#container");
 function drawGrid(size) {
-    gridSize = size * 4;
-    container.style.cssText = `width: ${gridSize}px; height: ${gridSize}px`;
+    container.style.cssText = `width: 800px; height: 800px`;
     for (let i = 0; i < size; i++) {
         const column = document.createElement("div");
         for (let j = 0; j < size; j++) {
             const square = document.createElement("div");
             square.addEventListener("mouseover", () => {
                 square.style.backgroundColor = "black";
-            })
-            square.style.cssText = "width: 4px; height: 4px;";
+            });
+            squareSize = 800 / size;
+            square.style.cssText = `width: ${squareSize}px; height: ${squareSize}px;`;
             column.appendChild(square);
         }
         container.appendChild(column);
@@ -28,4 +28,4 @@ function resize() {
 const resizeBtn = document.querySelector("#resizeBtn");
 resizeBtn.addEventListener("click", () => { resize(); });
 
-drawGrid(100);
+drawGrid(50);
